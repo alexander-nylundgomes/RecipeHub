aa0364af0ca9f6b5a95a2a0cda8b5aa042294b9f
Alexander Nylund Gomes <alexander.nylundgomes@nodeark.com>
2024-10-29 23:00:10 +0100

    Improvements

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RecipesComponent } from './pages/recipes/recipes.component';
+import { RecipeComponent } from './pages/recipe/recipe.component';
 
 export const routes: Routes = [
     {path: "", component: RecipesComponent},
+    {path: "recipe/:id", component: RecipeComponent},
 ];
--- src/app/state/recipes/recipes.selectors.ts
+++ src/app/state/recipes/recipes.selectors.ts
@@ -2,6 +2,11 @@ import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { Recipe } from '../../interfaces/recipe';
 
 export const selectRecipes = createFeatureSelector<ReadonlyArray<Recipe>>('recipes');
+
+export const selectRecipe = (id: number) => createSelector(selectRecipes, (recipes) => {
+    return recipes.find(recipe => recipe.id === id);
+}) 
+
 export const selectUsedIngredients = createSelector(selectRecipes, (recipes) => {
     recipes.map(recipe => recipe.ingredients);
 })
